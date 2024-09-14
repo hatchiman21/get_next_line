c2debea2bb54fc39401f49f52fea4d4cf1fb4183
aatieh_42 <amjdsalh@gmail.com>
2024-09-14 20:58:44 +0300

    updated to handle most cases

--- get_next_line.c
+++ get_next_line.c
@@ -1,22 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aatieh <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/14 16:30:44 by aatieh            #+#    #+#             */
+/*   Updated: 2024/09/14 16:30:47 by aatieh           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
-
-void	ft_lstclear(t_next_line **lst)
-{
-	t_next_line	*temp;
-	t_next_line	*temp2;
-
-	if (!*lst)
-		return ;
-	temp = *lst;
-	while (temp)
-	{
-		free (temp->buffer);
-		temp2 = temp;
-		temp = temp->next;
-		free (temp2);
-	}
-	*lst = NULL;
-}
+#include <string.h>
 
 char	*merge(char *res, char *word)
 {
@@ -34,7 +29,7 @@ int	ft_strchr(const char *s, int c)
 	int	i;
 
 	i = 0;
-	while ((i - 1 < 0 || s[i - 1]))
+	while (( s[i]))
 	{
 		if (s[i] == (char)c)
 			return (i + 1);
@@ -43,90 +38,91 @@ int	ft_strchr(const char *s, int c)
 	return (-1);
 }
 
-static char	*get_result(int fd, t_next_line *current, int i, char *res, t_next_line *all)
-{
-	char	*buff;
+
+
+
+static char	*get_result(int fd, char *buff, int i, char *res, int *used_buffer)
+{	
 	int		bytes_read;
 
 	bytes_read = 1;
+	*used_buffer = 1;
 	while (i == -1 && bytes_read > 0)
 	{
-		bytes_read = read(fd, current->buffer, BUFFER_SIZE);
-		if (bytes_read == -1)
-		{
-			free (res);
-			return (NULL);
-		}
+		bytes_read = read(fd, buff, BUFFER_SIZE);
 		if (bytes_read == 0)
 		{
-			ft_lstclear(&all);
+			*used_buffer = -1;
+			free (buff);
+			buff = NULL;
 			return (res);
 		}
-		current->buffer[BUFFER_SIZE] = '\0';
-		buff = current->buffer;
+		buff[bytes_read] = '\0';
 		i = ft_strchr(buff, '\n');
 		if (i != -1)
 		{
 			res = merge(res, ft_substr(buff, 0, i));
-			current->buffer = ft_memmove(buff, (buff + i),
-				ft_strlen(buff + i) + 1);
+			if (!(buff + i + 1))
+				*used_buffer = 0;
+			ft_memmove(buff, (buff + i), ft_strlen(buff + i) + 1);
 			return (res);
 		}
 		res = merge(res, ft_substr(buff, 0, ft_strlen(buff)));
+		if (ft_strlen(buff) == 0 && bytes_read < BUFFER_SIZE)
+		{
+			free (buff);
+			buff = NULL;
+		}
 	}
 	return (res);
 }
 
-static t_next_line	*get_buffer(t_next_line **all_fd, int fd)
+void	assgine_res(char **res)
 {
-	t_next_line	*check;
-	t_next_line	*new;
-
-	check = *all_fd;
-	while (check)
-	{
-		if (check->fd == fd)
-			return (check);
-		check = check->next;
-	}
-	new = (t_next_line *)malloc(sizeof(t_next_line));
-	if (!new)
-		return (NULL);
-	new->fd = fd;
-	new->buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	if (!(new->buffer))
-	{
-		free(new->buffer);
-		return (NULL);
-	}
-	new->buffer[0] = '\0';
-	new->next = *all_fd;
-	*all_fd = new;
-	return (new);
+	*res = (char *)malloc(sizeof(char));
+	*res[0] = '\0';
 }
 
 char	*get_next_line(int fd)
 {
-	static t_next_line	*all_fd;
-	t_next_line			*current;
-	int					i;
-	char				*res;
+	static char	*buffer;
+	static	int	used_buffer;
+	int			i;
+	char		*res;
 
 	i = -1;
+	if (!used_buffer)
+		used_buffer = -1;
 	if (fd == -1)
 		return (NULL);
-	res = (char *)malloc(sizeof(char));
-	current = get_buffer(&all_fd, fd);
-	res[0] = '\0';
-	if (current->buffer[0] != '\0')
-		i = ft_strchr(current->buffer, '\n');
+	if (used_buffer == -1)
+	{
+		used_buffer = 0;
+		buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+		buffer[0] = '\0';
+	}
+	if (read(fd, buffer, 0) == -1)
+	{
+		used_buffer = -1;
+		free (buffer);
+		buffer = NULL;
+		return (NULL);
+	}
+	assgine_res(&res);
+	if (used_buffer == 1)
+		i = ft_strchr(buffer, '\n');
 	if (i != -1)
 	{
-		res = ft_substr(current->buffer, 0, i);
-		current->buffer = ft_memmove(current->buffer, (current->buffer + i),
-				ft_strlen(current->buffer + i) + 1);
+		res = merge(res, ft_substr(buffer, 0, i));
+		ft_memmove(buffer, (buffer + i), ft_strlen(buffer + i) + 1);
 		return (res);
 	}
-	res = get_result(fd, current, i, res, all_fd);
+	else
+	res = get_result(fd, buffer, i, res, &used_buffer);
+	if (ft_strlen(res) == 0)
+	{
+		free (res);
+		return (NULL);
+	}
 	return (res);
 }
--- main.c
+++ main.c
@@ -1,20 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aatieh <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/14 16:31:03 by aatieh            #+#    #+#             */
+/*   Updated: 2024/09/14 16:31:07 by aatieh           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 #include <stdio.h>
 #include <fcntl.h>
+#include <string.h>
 
 int main()
 {
-    char *res;
-    int fd;
-	int	i;
+	char	*res;
+	int		fd;
+	int		i;
 
 	i = 0;
-    fd = open("test.txt", O_RDWR);
-	while (i++ < 5)
+	fd = open("test.txt", O_RDWR);
+	while (i++ < 2)
 	{
 		res = get_next_line(fd);
-    	printf("%d : %s",i,res);
+		printf("%d : %s \n", i, res);
 		free(res);
 	}
-    return 0;
+	return 0;
 }
